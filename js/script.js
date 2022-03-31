006d1fc865b78f51ca501365200c49511008a32e
Denys Voronov <kiberking11@gmail.com>
2022-03-31 16:32:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+   console.log('arr' + "-object");
+   console.log(4 + "5");
+let incr = 10,
+decr = 10;
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(16%6);
+
+console.log(2+2 *2 === 8);
+
+const isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose);
